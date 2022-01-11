389021a1a49779c5ecaaefdeee0f522586e3169f
DmitryPolyan <dmitrystomper@gmail.com>
2022-01-12 00:47:11 +0300

    handlers are done (without copytree!)

--- veeam_tt/main.py
+++ veeam_tt/main.py
@@ -1,7 +1,10 @@
 import argparse
 import os
-import app_logger
+import time
 
+import app_logger
+import filecmp
+import shutil
 
 logger = app_logger.get_logger(__name__)
 
@@ -14,17 +17,19 @@ def get_arguments() -> tuple:
     def check_path(path: str) -> str:
         """Checking a folder """
         if not os.path.isdir(path):
+            logger.error("Folder does not exist")
             raise argparse.ArgumentTypeError("Folder does not exist")
+        while path[-1] == '/':
+            path = path[:-1]
         return path
 
     def check_timing(timing: str) -> int:
         """ Checking that a digit is entered  """
         if timing.isdigit():
             return int(timing)
+        logger.error("Not a number entered")
         raise argparse.ArgumentTypeError("Not a number entered")
 
-    logger.info("123123")
-    logger.error("11111")
     parser = argparse.ArgumentParser(
         description="The utility copies files according to the provided configuration file.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -36,8 +41,57 @@ def get_arguments() -> tuple:
     return args.origin_folder, args.repl_folder, args.timing_for_refresh
 
 
+def update_repl_content(origin_folder: str, repl_folder: str, origin_name: str) -> None:
+    """ Updating changed data """
+    if os.path.isdir(f"{origin_folder}/{origin_name}"):
+        logger.info(f"Checking folder {origin_folder}/{origin_name}")
+        compare_folder_contents(f"{origin_folder}/{origin_name}", f"{repl_folder}/{origin_name}")
+    else:
+        logger.info(f"Copy {origin_folder}/{origin_name} to {repl_folder}")
+        shutil.copy2(f"{origin_folder}/{origin_name}", f"{repl_folder}")
+
+
+def append_new_content_int_repl(origin_folder: str, origin_name: str, repl_folder: str) -> None:
+    """ Add new data from source folder to replica folder """
+    if os.path.isdir(f"{origin_folder}/{origin_name}"):
+        logger.info(f"Copy folder {origin_folder}/{origin_name} to {repl_folder}")
+        # shutil.copytree(f"{origin_folder}/{origin_name}", f"{repl_folder}/")
+    else:
+        logger.info(f"Copy {origin_folder}/{origin_name} to {repl_folder}")
+        shutil.copy2(f"{origin_folder}/{origin_name}", f"{repl_folder}")
+
+
+def del_unnecessary_content(unnecessary_data_names: list, repl_folder: str) -> None:
+    """ Removing from replica folder data that is not in the original folder """
+    for i in unnecessary_data_names:
+        if os.path.isdir(f"{repl_folder}/{i}"):
+            logger.info(f"Delete {repl_folder}/{i} folder")
+            os.rmdir(f"{repl_folder}/{i}")
+        else:
+            logger.info(f"Delete {repl_folder}/{i} file")
+            os.remove(f"{repl_folder}/{i}")
+
+
+def compare_folder_contents(origin_folder: str, repl_folder: str) -> None:
+    content_origin_folder = os.listdir(origin_folder)
+    content_repl_folder = os.listdir(repl_folder)
+    for origin_name in content_origin_folder:
+        if origin_name in content_repl_folder:
+            content_repl_folder.remove(origin_name)
+            if not filecmp.cmp(f"{origin_folder}/{origin_name}", f"{repl_folder}/{origin_name}", shallow=False):
+                update_repl_content(origin_folder, repl_folder, origin_name)
+        elif origin_name not in content_repl_folder:
+            append_new_content_int_repl(origin_folder, origin_name, repl_folder)
+    del_unnecessary_content(content_repl_folder, repl_folder)
+
+
 def main():
+    logger.info("Application started")
     origin_folder, repl_folder, timing_for_refresh = get_arguments()
+    logger.info(f"origin_folder {origin_folder}, repl_folder {repl_folder}, timing_for_refresh {timing_for_refresh}")
+    while True:
+        time.sleep(timing_for_refresh)
+        compare_folder_contents(origin_folder, repl_folder)
 
 
 if __name__ == '__main__':
