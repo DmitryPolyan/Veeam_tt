9d8a0998298c45132855dae02724552300d3b81c
DmitryPolyan <dmitrystomper@gmail.com>
2022-01-12 14:12:32 +0300

    is done (without tests)

--- veeam_tt/main.py
+++ veeam_tt/main.py
@@ -11,7 +11,7 @@ logger = app_logger.get_logger(__name__)
 
 def get_arguments() -> tuple:
     """
-    Getting paths to folders and timing to check from the command line.
+    Getting paths to folders and refresh time period from the command line.
     :return: tuple(path_origin: str, path_repl: str, time_of_check: int)
     """
     def check_path(path: str) -> str:
@@ -23,10 +23,10 @@ def get_arguments() -> tuple:
             path = path[:-1]
         return path
 
-    def check_timing(timing: str) -> int:
+    def check_refresh_time_period(refresh_time_period: str) -> int:
         """ Checking that a digit is entered  """
-        if timing.isdigit():
-            return int(timing)
+        if refresh_time_period.isdigit():
+            return int(refresh_time_period)
         logger.error("Not a number entered")
         raise argparse.ArgumentTypeError("Not a number entered")
 
@@ -36,25 +36,25 @@ def get_arguments() -> tuple:
     )
     parser.add_argument("origin_folder", type=check_path, help="Path to origin folder")
     parser.add_argument("repl_folder", type=check_path, help="Path to replica folder")
-    parser.add_argument("timing_for_refresh", type=check_timing, help="Timing for refresh folders")
+    parser.add_argument("refresh_time_period", type=check_refresh_time_period, help="Time period for refresh folders")
     args = parser.parse_args()
-    return args.origin_folder, args.repl_folder, args.timing_for_refresh
+    return args.origin_folder, args.repl_folder, args.refresh_time_period
 
 
-def update_repl_content(origin_folder: str, repl_folder: str, origin_name: str) -> None:
+def update_repl_content(origin_folder: str, repl_folder: str, original_name: str) -> None:
     """ Updating changed data """
-    logger.info(f"Copy {origin_folder}/{origin_name} to {repl_folder}")
-    shutil.copy2(f"{origin_folder}/{origin_name}", f"{repl_folder}")
+    logger.info(f"Copy {origin_folder}/{original_name} to {repl_folder}")
+    shutil.copy2(f"{origin_folder}/{original_name}", f"{repl_folder}")
 
 
-def append_new_content_int_repl(origin_folder: str, origin_name: str, repl_folder: str) -> None:
+def append_new_content_int_repl(origin_folder: str, original_name: str, repl_folder: str) -> None:
     """ Add new data from source folder to replica folder """
-    if os.path.isdir(f"{origin_folder}/{origin_name}"):
-        logger.info(f"Copy folder {origin_folder}/{origin_name} to {repl_folder}")
-        # shutil.copytree(f"{origin_folder}/{origin_name}", f"{repl_folder}/")
+    if os.path.isdir(f"{origin_folder}/{original_name}"):
+        logger.info(f"Copy folder {origin_folder}/{original_name} to {repl_folder}")
+        shutil.copytree(f"{origin_folder}/{original_name}", f"{repl_folder}/{original_name}")
     else:
-        logger.info(f"Copy {origin_folder}/{origin_name} to {repl_folder}")
-        shutil.copy2(f"{origin_folder}/{origin_name}", f"{repl_folder}")
+        logger.info(f"Copy {origin_folder}/{original_name} to {repl_folder}")
+        shutil.copy2(f"{origin_folder}/{original_name}", f"{repl_folder}")
 
 
 def del_unnecessary_content(unnecessary_data_names: list, repl_folder: str) -> None:
@@ -62,37 +62,38 @@ def del_unnecessary_content(unnecessary_data_names: list, repl_folder: str) -> N
     for i in unnecessary_data_names:
         if os.path.isdir(f"{repl_folder}/{i}"):
             logger.info(f"Delete {repl_folder}/{i} folder")
-            os.rmdir(f"{repl_folder}/{i}")
+            shutil.rmtree(f"{repl_folder}/{i}", ignore_errors=True)
         else:
             logger.info(f"Delete {repl_folder}/{i} file")
             os.remove(f"{repl_folder}/{i}")
 
 
-def compare_folder_contents(origin_folder: str, repl_folder: str) -> None:
+def handling_folder_contents(origin_folder: str, repl_folder: str) -> None:
+    """Comparison of content in folders and handling"""
     content_origin_folder = os.listdir(origin_folder)
     content_repl_folder = os.listdir(repl_folder)
-    for origin_name in content_origin_folder:
-        if origin_name in content_repl_folder:
-            content_repl_folder.remove(origin_name)
-            if os.path.isdir(f"{origin_folder}/{origin_name}"):
-                logger.info(f"Checking folder {origin_folder}/{origin_name}")
-                compare_folder_contents(f"{origin_folder}/{origin_name}", f"{repl_folder}/{origin_name}")
+    for original_name in content_origin_folder:
+        if original_name in content_repl_folder:
+            content_repl_folder.remove(original_name)
+            if os.path.isdir(f"{origin_folder}/{original_name}"):
+                logger.info(f"Checking folder {origin_folder}/{original_name}")
+                handling_folder_contents(f"{origin_folder}/{original_name}", f"{repl_folder}/{original_name}")
             else:
-                if not filecmp.cmp(f"{origin_folder}/{origin_name}", f"{repl_folder}/{origin_name}", shallow=False):
-                    update_repl_content(origin_folder, repl_folder, origin_name)
-        elif origin_name not in content_repl_folder:
-            append_new_content_int_repl(origin_folder, origin_name, repl_folder)
+                if not filecmp.cmp(f"{origin_folder}/{original_name}", f"{repl_folder}/{original_name}", shallow=False):
+                    update_repl_content(origin_folder, repl_folder, original_name)
+        elif original_name not in content_repl_folder:
+            append_new_content_int_repl(origin_folder, original_name, repl_folder)
     del_unnecessary_content(content_repl_folder, repl_folder)
 
 
 def main():
     logger.info("Application started")
-    origin_folder, repl_folder, timing_for_refresh = get_arguments()
-    logger.info(f"origin_folder {origin_folder}, repl_folder {repl_folder}, timing_for_refresh {timing_for_refresh}")
-    # while True:
-    #     time.sleep(timing_for_refresh)
-    #     compare_folder_contents(origin_folder, repl_folder)
-    compare_folder_contents(origin_folder, repl_folder)
+    origin_folder, repl_folder, refresh_time_period = get_arguments()
+    logger.info(f"origin_folder {origin_folder}, repl_folder {repl_folder}, refresh_time_period {refresh_time_period}")
+    while True:
+        handling_folder_contents(origin_folder, repl_folder)
+        time.sleep(refresh_time_period)
+        logger.info("Checking restart")
 
 
 if __name__ == '__main__':
