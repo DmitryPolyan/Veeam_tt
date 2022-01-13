cfdc528d424945c2abf06706ccd90b0010560304
DmitryPolyan <dmitrystomper@gmail.com>
2022-01-13 13:43:27 +0300

    add log path

--- veeam_tt/app_logger.py
+++ veeam_tt/app_logger.py
@@ -3,26 +3,27 @@ import logging
 log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
 
-def file_handler() -> logging.FileHandler:
-    """Handler for writing in file """
-    file_handler = logging.FileHandler("logs.log")
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter(log_format))
-    return file_handler
+class Logger:
+    def __init__(self, name, log_path):
+        """Logger creation """
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        logger.addHandler(Logger.file_handler(log_path))
+        logger.addHandler(Logger.stream_handler())
 
+    @staticmethod
+    def file_handler(log_path: str) -> logging.FileHandler:
+        """Handler for writing in file """
+        file_handler = logging.FileHandler(f"{log_path}/logs.log")
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter(log_format))
+        return file_handler
 
-def stream_handler() -> logging.StreamHandler:
-    """Handler for console output """
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(logging.Formatter(log_format))
-    return stream_handler
+    @staticmethod
+    def stream_handler() -> logging.StreamHandler:
+        """Handler for console output """
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(logging.Formatter(log_format))
+        return stream_handler
 
-
-def get_logger(name: str) -> logging.getLogger:
-    """Logger creation """
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(file_handler())
-    logger.addHandler(stream_handler())
-    return logger
--- veeam_tt/main.py
+++ veeam_tt/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import time
 import filecmp
@@ -11,14 +12,15 @@ cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(insp
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
-import app_logger
-logger = app_logger.get_logger(__name__)
+from app_logger import Logger
+
+logger = logging.getLogger(__name__)
 
 
 def get_arguments() -> tuple:
     """
     Getting paths to folders and refresh time period from the command line.
-    :return: tuple(path_origin: str, path_repl: str, time_of_check: int)
+    :return: tuple(path_origin: str, path_repl: str, time_of_check: int, log_path: str)
     """
     def check_path(path: str) -> str:
         """Checking a folder """
@@ -43,8 +45,9 @@ def get_arguments() -> tuple:
     parser.add_argument("origin_folder", type=check_path, help="Path to origin folder")
     parser.add_argument("repl_folder", type=check_path, help="Path to replica folder")
     parser.add_argument("refresh_time_period", type=check_refresh_time_period, help="Time period for refresh folders")
+    parser.add_argument('log_path', type=check_path, help='Path to logs.log')
     args = parser.parse_args()
-    return args.origin_folder, args.repl_folder, args.refresh_time_period
+    return args.origin_folder, args.repl_folder, args.refresh_time_period, args.log_path
 
 
 def update_repl_content(origin_folder: str, repl_folder: str, original_name: str) -> None:
@@ -93,9 +96,11 @@ def handling_folder_contents(origin_folder: str, repl_folder: str) -> None:
 
 
 def main():
+    origin_folder, repl_folder, refresh_time_period, log_path = get_arguments()
+    Logger(__name__, log_path)
     logger.info("Application started")
-    origin_folder, repl_folder, refresh_time_period = get_arguments()
-    logger.info(f"origin_folder {origin_folder}, repl_folder {repl_folder}, refresh_time_period {refresh_time_period}")
+    logger.info(f"origin folder {origin_folder}, repl folder {repl_folder}, refresh time period {refresh_time_period}"
+                f" log path {log_path}")
     while True:
         handling_folder_contents(origin_folder, repl_folder)
         time.sleep(refresh_time_period)
